aa4249488e8f319b426f527784db23ab8d5b745e
Lenovo <Lenovo@DESKTOP-POA1PA7>
2022-12-12 22:22:19 +0530

    First Commit..............

---
+++ src/Print_println.java
@@ -0,0 +1,15 @@
+
+
+public class Print_println {
+
+	public static void main(String[] args) {
+		System.out.println("am leraning java");
+		System.out.println("am leraning selenium");
+		System.out.println("am leraning testing");
+		System.out.print("am leraning java");
+		System.out.print("am leraning selenium");
+		System.out.print("am leraning testing");
+
+	}
+
+}
